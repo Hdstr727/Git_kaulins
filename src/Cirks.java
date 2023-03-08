d1f9cb2dd829ed1f0006613a66e05d2197154c20
Travinovs <aleksandrs.travinovs@gmail.com>
2023-03-08 09:39:23 +0200

    Ievesta iespeja mest kaulinu vairakas reizes

--- src/Cirks.java
+++ src/Cirks.java
@@ -2,14 +2,17 @@ import java.util.Random;
 
 public class Cirks {
 	
-	static void mestKaulinu() {
+	static void mestKaulinu(int reizes) {
 		int skaitlis;
 		Random rand = new Random();
-		skaitlis = rand.nextInt(6)+1;
-		System.out.println("Uzkrita skaitlis"+skaitlis);
+		
+		for(int i=0; i<reizes; i++) {
+ 			skaitlis = rand.nextInt(6)+1;
+				System.out.println("Uzkrita skaitlis: "+skaitlis);
+		}
 	}
 	public static void main(String[] args) {
-		mestKaulinu();
+		mestKaulinu(3);
 		
 	}
 
