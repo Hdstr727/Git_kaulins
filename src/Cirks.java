096d57f3e60961e019f4aa18bb2f636627c89c63
Travinovs <aleksandrs.travinovs@gmail.com>
2023-03-20 16:03:09 +0200

    Lokalaja kZarā papildinata programma

--- src/Cirks.java
+++ src/Cirks.java
@@ -4,22 +4,25 @@ import java.util.Scanner;
 public class Cirks {
 	
 	static void mestKaulinu(int reizes) {
-		int skaitlis;
+		int skaitlis, lielakais = 0;
 		Random rand = new Random();
 		for(int i=1; i<=reizes; i++) {
  			skaitlis = rand.nextInt(6)+1;
 				System.out.println("Uzkrita skaitlis: "+skaitlis);
+				
+				if(lielakais<skaitlis)
+					lielakais = skaitlis;
 		}
+		System.out.println("Lielakais skaitlis metienos ir "+lielakais);
 	}
 	public static void main(String[] args) {
 		int reizes;
 		Scanner scan = new Scanner(System.in);
 		do {
-		System.out.println("Cik reizes mest kaulinu?");
-		reizes = scan.nextInt();
+			System.out.println("Cik reizes mest kaulinu?");
+			reizes = scan.nextInt();
 		} while(reizes<1);
 		mestKaulinu(reizes);
 		scan.close();
 	}
-
 }
